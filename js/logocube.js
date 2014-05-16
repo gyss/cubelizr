6a1b0ffc334e7058f26dbdcdf1bcb0e92bff435c
Gravedian <gravedian@gmail.com>
2014-05-16 23:34:56 +0200

    default values

--- js/logocube.js
+++ js/logocube.js
@@ -5,33 +5,140 @@
 	var camera, scene, renderer;
 
 
-	var cube;
 	var direction = 1;
 
-	var cubes = [];
+	var world_objects = [];
 
-//http://www.phpied.com/3-ways-to-define-a-javascript-class/
+	/* Types of animation: http://sole.github.io/tween.js/examples/03_graphs.html */
+
 
 	init();
 	animate();
 
 
-
 	// Retrieve json configuration file
-	jQuery.getJSON("data.json", function( data ) {
+	jQuery.getJSON("logocube.json", function( data ) {
 
+		// First, load default values
+		var type = data.defaults.type ? data.defaults.type : "cube";
+		var color = data.defaults.color ? data.defaults.color : "0xff22ff";
+		var opacity = data.defaults.opacity ? data.defaults.opacity : "0xff22ff";
+		var start = data.defaults.start ? data.defaults.start : 0.1;
+		var duration = data.defaults.duration ? data.defaults.duration : 2.0;
+		var animation = data.defaults.animation ? data.defaults.animation : "TWEEN.Easing.Elastic.InOut";
+		var height = data.defaults.height ? data.defaults.height : 100;
+		
 		$.each( data.objects , function( key, val ) {
-			
-			console.log(val);
 
-			var duration = val.duration ? val.duration : 2;
-			var start = val.start ? val.start : 0.1;
+			// Second, create elements into the world
+			var obj_type = val.type ? val.type : type;
+			var obj_color = val.color ? val.color : color;
+			var obj_opacity = val.opacity ? val.opacity : opacity;
+			var obj_start = val.start ? val.start : start;
+			var obj_duration = val.duration ? val.duration : duration;
+			var obj_animation = val.animation ? dval.animation : animation;
+			var obj_x = val.x ? val.x : 0;
+			var obj_y = val.y ? val.y : 0;
+			var obj_height = val.height ? val.height : height;
 
-			create_cube(val.x, val.y, val.start, duration); 
-			
+			// 1. Create object
+			var object = object_factory(obj_type, obj_color, obj_opacity);
+
+			// 2. Set initial position
+			translate_object(object, obj_x, obj_y, obj_height);
+
+			// 3. Configure animation
+			animate_object(object, obj_height, obj_start, obj_duration, obj_animation);
+
+			// 4. Store object
+			world_objects.push(object);
+
 		});
 		
 	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	function animate_object(obj, height, start, duration, animation) {
+
+		// - Tween -
+		var position = { y: height };
+		var target = { y: 0 };
+		var tween = new TWEEN.Tween(position).to(target, duration*1000);
+
+		tween.delay(start * 100);
+
+		tween.easing(tween_animation(animation));
+
+		tween.onUpdate(function() {
+			obj.mesh.position.y = position.y;
+		});
+
+		tween.start();
+		
+		obj.tween = tween;
+
+	}
+
+	function object_factory(type, color, opacity) {
+
+		var new_obj = {};
+
+		var geometry;
+		var mesh;
+
+		// - 3dObject -
+		switch(type)
+		{
+			case "cube":
+				geometry = new THREE.BoxGeometry( TILE_SIZE, TILE_SIZE, TILE_SIZE ); break;
+		}
+		
+		var material = new THREE.MeshBasicMaterial({ 
+			color: color, 
+			opacity: opacity, 
+			side: THREE.DoubleSide
+		});
+
+		mesh = new THREE.Mesh( geometry, material );
+
+		scene.add( mesh );
+		new_obj.mesh = mesh;
+
+		return new_obj;
+	}
+
+	function translate_object(obj, x, y, height) {
+
+		obj.mesh.position.x = TILE_SIZE * x ;
+		obj.mesh.position.y = height;
+		obj.mesh.position.z = TILE_SIZE * y ;
+
+	}
+
+	
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -102,10 +209,7 @@
 	{
 		// Grid
 		init_grid(scene);
-		
-		// Cubes
-		//init_cubes(scene);
-		
+
 		// Lights
 		init_light(scene);
 	}
@@ -143,83 +247,6 @@
 		scene.add( line );
 	}
 
-
-	//
-	// Cubes Initialization
-	//
-	function init_cubes(scene) {
-		
-		var geometry = new THREE.BoxGeometry( TILE_SIZE, TILE_SIZE, TILE_SIZE );
-		var material = new THREE.MeshLambertMaterial( { color: 0xffffff, shading: THREE.FlatShading, overdraw: 0.5 } );
-
-		/*
-		for ( var i = 0; i < 5; i ++ ) {
-
-			var cube = new THREE.Mesh( geometry, material );
-
-			cube.scale.y = Math.floor( Math.random() * 2 + 1 );
-
-			cube.position.x = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
-			cube.position.y = ( cube.scale.y * 50 ) / 2;
-			cube.position.z = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
-
-			scene.add( cube );
-
-		}
-		*/
-
-		cube = new THREE.Mesh( geometry, material );
-		//cube.scale.y = Math.floor( Math.random() * 2 + 1 );
-
-		cube.position.x =  0;
-		cube.position.y = 100;
-		cube.position.z =  0;
-
-		scene.add( cube );
-
-	}
-
-	function create_cube(x, y, start, duration) {
-
-		var new_cube = {};
-
-		// - 3dObject
-		var geometry = new THREE.BoxGeometry( TILE_SIZE, TILE_SIZE, TILE_SIZE );
-		var material = new THREE.MeshLambertMaterial( { color: 0xffffff, shading: THREE.FlatShading, overdraw: 0.5 } );
-
-		cube = new THREE.Mesh( geometry, material );
-
-		cube.position.x = TILE_SIZE * x ;
-		cube.position.y = 100;
-		cube.position.z = TILE_SIZE * y ;
-
-		scene.add( cube );
-
-		new_cube.cube = cube;
-
-		// - Tween -
-		var position = { y: 100 };
-		var target = { y: 0 };
-		var tween = new TWEEN.Tween(position).to(target, duration*1000);
-
-		tween.delay(start * 100);
-
-		tween.easing(TWEEN.Easing.Elastic.InOut);
-
-		tween.onUpdate(function() {
-			new_cube.cube.position.y = position.y;
-		});
-
-		tween.start();
-		
-		new_cube.tween = tween;
-
-
-		// Add cube to the list
-		cubes.push(new_cube);
-
-	}
-
 	//
 	// Light Initialization
 	//
@@ -247,6 +274,17 @@
 
 
 
+
+
+
+
+
+
+
+	
+
+	
+
 	function onWindowResize() {
 
 		camera.left = window.innerWidth / - 2;
@@ -259,12 +297,6 @@
 		renderer.setSize( window.innerWidth, window.innerHeight );
 
 	}
-
-
-	
-
-	//
-
 
 	function animate() {
 
@@ -279,6 +311,7 @@
 
 	function render() {
 
+
 		/*
 		if(cube.position.y > -200) {
 			direction = -1;
@@ -287,16 +320,55 @@
 		}
 		*/
 
-		//console.log(cube.position.x + " = " + cube.position.y + " = " + cube.position.z);
 		//cube.position.y += (1 * direction);
-
-		
 		//cube.position.y++;
+
+
+
 
 		TWEEN.update();
 
 		var timer = Date.now() * 0.0001;				
 		renderer.render( scene, camera );
 
+	}
+
+	function tween_animation(animation_str)
+	{
+		switch(animation_str)
+		{
+			case "Linear.None": return TWEEN.Easing.Linear.None; break;
+			case "Quadratic.In": return TWEEN.Easing.Quadratic.In; break;
+			case "Quadratic.Out": return TWEEN.Easing.Quadratic.Out; break;
+			case "Quadratic.InOut": return TWEEN.Easing.Quadratic.InOut; break;
+			case "Cubic.In": return TWEEN.Easing.Cubic.In; break;
+			case "Cubic.Out": return TWEEN.Easing.Cubic.Out; break;
+			case "Cubic.InOut": return TWEEN.Easing.Cubic.InOut; break;
+			case "Quartic.In": return TWEEN.Easing.Quartic.In; break;
+			case "Quartic.Out": return TWEEN.Easing.Quartic.Out; break;
+			case "Quartic.InOut": return TWEEN.Easing.Quartic.InOut; break;
+			case "Quintic.In": return TWEEN.Easing.Quintic.In; break;
+			case "Quintic.Out": return TWEEN.Easing.Quintic.Out; break;
+			case "Quintic.InOut": return TWEEN.Easing.Quintic.InOut; break;
+			case "Sinusoidal.In": return TWEEN.Easing.Sinusoidal.In; break;
+			case "Sinusoidal.Out": return TWEEN.Easing.Sinusoidal.Out; break;
+			case "Sinusoidal.InOut": return TWEEN.Easing.Sinusoidal.InOut; break;
+			case "Exponential.In": return TWEEN.Easing.Exponential.In; break;
+			case "Exponential.Out": return TWEEN.Easing.Exponential.Out; break;
+			case "Exponential.InOut": return TWEEN.Easing.Exponential.InOut; break;
+			case "Circular.In": return TWEEN.Easing.Circular.In; break;
+			case "Circular.Out": return TWEEN.Easing.Circular.Out; break;
+			case "Circular.InOut": return TWEEN.Easing.Circular.InOut; break;
+			case "Elastic.In": return TWEEN.Easing.Elastic.In; break;
+			case "Elastic.Out": return TWEEN.Easing.Elastic.Out; break;
+			case "Elastic.InOut": return TWEEN.Easing.Elastic.InOut; break;
+			case "Back.In": return TWEEN.Easing.Back.In; break;
+			case "Back.Out": return TWEEN.Easing.Back.Out; break;
+			case "Back.InOut": return TWEEN.Easing.Back.InOut; break;
+			case "Bounce.In": return TWEEN.Easing.Bounce.In; break;
+			case "Bounce.Out": return TWEEN.Easing.Bounce.Out; break;
+			case "Bounce.InOut": return TWEEN.Easing.Bounce.InOut; break;
+			default: return TWEEN.Easing.Linear.None; 
+		}
 	}
 	
